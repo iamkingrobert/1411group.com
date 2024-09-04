0b47d36f3b767c4fc33f8015941f56678cb7b8d7
King Robert Kehinde <robert.k@techie.com>
2024-09-04 10:41:02 +0200

    Emailjs Integrated successfully, and more UI Updates.

--- src/Components/CustomerForm.jsx
+++ src/Components/CustomerForm.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
+import emailjs from "emailjs-com";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
-    fullName: "",
-    email: "",
+    from_name: "",
+    from_email: "",
     message: "",
     inquiryType: "PROJECT",
   });
+
+  const [successMessage, setSuccessMessage] = useState(""); // New state for success message
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -19,21 +22,41 @@ const ContactForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Example with EmailJS or your backend API here
-    // sendEmail(formData);
-
-    console.log("Form Submitted: ", formData);
-    alert("Form submitted successfully!");
+    emailjs
+      .send(
+        "service_vf7o9cf",
+        "template_o0bcy1j",
+        formData,
+        "8KXiSl_TS3VqB8S1r"
+      )
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+          setSuccessMessage(
+            "Message sent successfully, we will get back to you shortly."
+          ); // Show success message
+          setFormData({
+            from_name: "",
+            from_email: "",
+            message: "",
+            inquiryType: "PROJECT",
+          }); // Clear form
+        },
+        (error) => {
+          console.log("FAILED...", error);
+          alert("Form submission failed.");
+        }
+      );
   };
 
   return (
     <form
       onSubmit={handleSubmit}
-      className="max-w-[860px] h-[720px] md:mx-auto mx-4 p-8 md:p-10 bg-white shadow-lg shadow-[#4fc4cb] rounded-[18px] mb-[20px] relative -top-[100px]"
+      className="max-w-[860px] h-[745px] md:mx-auto mx-4 p-8 md:p-10 bg-white shadow-lg shadow-[#4fc4cb] rounded-[18px] mb-[20px] relative -top-[100px]"
     >
-     <div className="mb-4">
+      <div className="mb-4">
         <label
-          htmlFor="email"
+          htmlFor="fullName"
           className="block text-gray-700 text-sm font-bold mb-2"
         >
           FULL NAME: *
@@ -41,8 +64,8 @@ const ContactForm = () => {
         <input
           type="text"
           id="fullName"
-          name="fullName"
-          value={formData.fullName}
+          name="from_name"
+          value={formData.from_name}
           onChange={handleChange}
           placeholder="Full name*"
           required
@@ -60,8 +83,8 @@ const ContactForm = () => {
         <input
           type="email"
           id="email"
-          name="email"
-          value={formData.email}
+          name="from_email"
+          value={formData.from_email}
           onChange={handleChange}
           placeholder="Your email*"
           required
@@ -87,7 +110,7 @@ const ContactForm = () => {
         />
       </div>
 
-      <div className="mb-4 md:pt-5">
+      <div className="mb-3 md:pt-5">
         <label className="block text-gray-700 text-sm font-bold mb-2">
           TYPE OF INQUIRY
         </label>
@@ -128,13 +151,20 @@ const ContactForm = () => {
         </div>
       </div>
 
-      <div className="mb-4 pt-4 md:pt-0">
+      <div className="mb-5 pt-4 md:pt-5">
         <button
           type="submit"
-          className="px-6 py-3 text-white bg-[#171b2c] rounded hover:bg-[#4fc4cb] hover:text-[#171b2c]">
+          className="px-6 py-3 text-white bg-[#171b2c] rounded hover:bg-[#4fc4cb] hover:text-[#171b2c]"
+        >
           Send Message
         </button>
       </div>
+
+      {successMessage && (
+        <div className="mb-2 text-[#265457] font-normal text-[16px] text-center">
+          {successMessage}
+        </div>
+      )}
     </form>
   );
 };
